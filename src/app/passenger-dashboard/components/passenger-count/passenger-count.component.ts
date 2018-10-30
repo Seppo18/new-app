5005106d22d619cd23bb55c042d03f1963dde6fe
Seppo Salmela <seppo.salmela@landisgyr.com>
2018-10-30 19:11:57 +0200

    set 2
    Identification: jou
    Change-Id: I7844047d175cb27b75be65ed1ae2756326bc1fee

--- src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -3,11 +3,21 @@ import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'app-passenger-count',
-  templateUrl: './passanger-count.component.html'
+  templateUrl: 'passanger-count.component.html'
 })
 export class PassengerCountComponent {
   @Input()
   items: Passenger[];
 
   constructor() {}
+
+  checkedInCount(): Number {
+    if (!this.items) {
+      return;
+    }
+
+    return this.items.filter((p: Passenger) => {
+      return p.checkedIn;
+    }).length;
+  }
 }
--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, Output, EventEmitter  } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
-  selector: 'passenger-detail',
-  template: `
-    <div>
-      Detail component
-    </div>
-  `
+  selector: 'app-passenger-detail',
+  styleUrls: ['passenger-detail.component.scss'],
+  templateUrl: 'passenger-detail.component.html'
 })
-export class PassengerDetailComponent {
+export class PassengerDetailComponent implements OnInit, OnChanges {
+  @Input()
+  detail: Passenger;
+
+  @Output()
+  edit: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  remove: EventEmitter<any> = new EventEmitter();
+
+  editing: boolean = false;
+
   constructor() {}
+
+  ngOnChanges(changes) {
+    if (changes.detail) {
+      this.detail = Object.assign({}, changes.detail.currentValue);
+    }
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  onNameChange(value: string) {
+    this.detail.fullname = value;
+  }
+
+  toggleEdit() {
+    if (this.editing) {
+      this.edit.emit(this.detail);
+    }
+    this.editing = !this.editing;
+  }
+  onRemove() {
+    this.remove.emit(this.detail);
+  }
 }
