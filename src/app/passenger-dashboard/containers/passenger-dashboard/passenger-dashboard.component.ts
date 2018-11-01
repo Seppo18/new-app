98b65e0148f925a6a70fbef5b1de8e15695f85e0
Seppo Salmela <seppo.salmela@landisgyr.com>
2018-11-01 15:24:26 +0200

    no http client
    Identification:
    Change-Id: Ibda6b2bb4602374199e8607f803116fe9799be11

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
 import { Passenger } from "../../models/passenger.interface";
-import { PasssangerDashboardService } from "../../passenger-dashboard.service";
-import { Observable } from "rxjs";
 
 @Component({
   selector: "app-passenger-dashboard",
@@ -11,9 +9,45 @@ import { Observable } from "rxjs";
 })
 export class PassengerDashboardComponent implements OnInit {
   passengers: Passenger[];
-  constructor(private passangerService: PasssangerDashboardService) {}
+  constructor() {}
   ngOnInit() {
-    this.passangerService.getPassangers().subscribe((ob: Passenger[]) => { this.passengers = ob; });
+    this.passengers = [
+      {
+        id: 1,
+        fullname: "Stephen",
+        checkedIn: true,
+        checkInDate: 1490742000000,
+        children: null
+      },
+      {
+        id: 2,
+        fullname: "Rose",
+        checkedIn: false,
+        checkInDate: null,
+        children: [{ name: "Ted", age: 12 }, { name: "Chloe", age: 7 }]
+      },
+      {
+        id: 3,
+        fullname: "James",
+        checkedIn: true,
+        checkInDate: 1491606000000,
+        children: null
+      },
+      {
+        id: 4,
+        fullname: "Louise",
+        checkedIn: true,
+        checkInDate: 1488412800000,
+        children: [{ name: "Jessica", age: 1 }]
+      },
+      {
+        id: 5,
+        fullname: "Tina",
+        checkedIn: false,
+        checkInDate: null,
+        children: null
+      }
+    ];
   }
 
   handleEdit(event: Passenger) {
